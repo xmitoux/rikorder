24546e0dc52037059f0c5497c40c9cf7966e4f77
mitou <xmitoux@gmail.com>
2024-06-22 22:05:46 +0900

    chore: nuxtとnestのeslint-custom使用設定

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -1,7 +1,21 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// dev環境でのみ使用するモジュール
+const devModules = process.env.NODE_ENV === 'development'
+  ? ['@nuxt/eslint']
+  : [];
+
+const modules = [
+  ...devModules,
+];
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   devServer: {
       port: 3001
-  }
+  },
+  modules,
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 })
