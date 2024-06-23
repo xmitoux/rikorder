6b673cafff65b68ab2e3853df982b9bb395b3717
mitou <xmitoux@gmail.com>
2024-06-23 19:12:24 +0900

    chore(backend): pino設定

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const pinoHttp = {
+  level: process.env.PINO_LOG_LEVEL || 'trace',
+  transport: {
+    options: {
+      singleLine: true,
+      translateTime: 'SYS:yyyy-mm-dd HH:MM:ss.l',
+    },
+    target: 'pino-pretty',
+  },
+};
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    LoggerModule.forRoot({ pinoHttp }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  const logger = app.get(Logger);
+  app.useLogger(logger);
+
+  await app.listen(3000, () => {
+    logger.log(`Server listening on port:3000 NODE_ENV=${process.env.NODE_ENV}`);
+  });
 }
 
 // eslint-disable-next-line unicorn/prefer-top-level-await
