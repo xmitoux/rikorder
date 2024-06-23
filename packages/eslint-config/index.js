4b4590a6ce6846c9c2e94d7944f3da779c4f5817
mitou <xmitoux@gmail.com>
2024-06-23 23:39:58 +0900

    style: eslint rules

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -9,18 +9,18 @@ import stylistic from '@stylistic/eslint-plugin';
  * @type {import('eslint').Linter.FlatConfig}
  */
 export const customEslintRules = {
-    rules: {
-        'array-callback-return': ['error', { checkForEach: true }],
-        'import/order': 'off',
-        'no-console': 'error',
-        '@typescript-eslint/no-extraneous-class': 'off',
-        'vue/max-attributes-per-line': 'off',
-        'vue/attributes-order': ["error", { alphabetical: true }],
-    },
+  rules: {
+    'array-callback-return': ['error', { checkForEach: true }],
+    'import/order': 'off',
+    'no-console': 'error',
+    '@typescript-eslint/no-extraneous-class': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/attributes-order': ["error", { alphabetical: true }],
+  },
 };
 
 export const customIgnores = {
-    ignores: ['**/node_modules', '**/public', '**/vendor', '**/dist', '**/.nuxt'],
+  ignores: ['**/node_modules', '**/public', '**/vendor', '**/dist', '**/.nuxt'],
 };
 
 const configUnicornRecommended = eslintPluginUnicorn.configs['flat/recommended'];
@@ -29,83 +29,83 @@ const configUnicornRecommended = eslintPluginUnicorn.configs['flat/recommended']
  * @type {import('eslint').Linter.FlatConfig}
  */
 const customUnicornRules = {
-    rules: {
-        'unicorn/consistent-function-scoping': 'off',
-        'unicorn/filename-case': 'off',
-        'unicorn/prefer-module': 'off',
-        'unicorn/prevent-abbreviations': 'off',
-        'unicorn/no-null': 'off',
-    },
+  rules: {
+    'unicorn/consistent-function-scoping': 'off',
+    'unicorn/filename-case': 'off',
+    'unicorn/prefer-module': 'off',
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/no-null': 'off',
+  },
 };
 
 /**
  * @type {import('eslint').Linter.FlatConfig}
  */
 const configPerfectionist = {
-    plugins: {
-        perfectionist,
-    },
-    rules: {
-        'perfectionist/sort-exports': 'error',
-        'perfectionist/sort-imports': [
-            'error',
-            {
-                type: 'natural',
-                order: 'asc',
-                'custom-groups': {
-                    value: {
-                        repo: '@repo/**',
-                    },
-                },
-                groups: [
-                    'builtin',
-                    'external',
-                    'repo',
-                    'internal',
-                    'parent',
-                    'sibling',
-                    'side-effect',
-                    'side-effect-style',
-                    'index',
-                    'object',
-                    'style',
-                    'type',
-                    'builtin-type',
-                    'external-type',
-                    'internal-type',
-                    'parent-type',
-                    'sibling-type',
-                    'index-type',
-                    'unknown',
-                ],
-                'newlines-between': 'always',
-                'internal-pattern': [
-                    '@/**',
-                    '~/**'
-                ],
-            },
+  plugins: {
+    perfectionist,
+  },
+  rules: {
+    'perfectionist/sort-exports': 'error',
+    'perfectionist/sort-imports': [
+      'error',
+      {
+        type: 'natural',
+        order: 'asc',
+        'custom-groups': {
+          value: {
+            repo: '@repo/**',
+          },
+        },
+        groups: [
+          'builtin',
+          'external',
+          'repo',
+          'internal',
+          'parent',
+          'sibling',
+          'side-effect',
+          'side-effect-style',
+          'index',
+          'object',
+          'style',
+          'type',
+          'builtin-type',
+          'external-type',
+          'internal-type',
+          'parent-type',
+          'sibling-type',
+          'index-type',
+          'unknown',
         ],
-    },
+        'newlines-between': 'always',
+        'internal-pattern': [
+          '@/**',
+          '~/**'
+        ],
+      },
+    ],
+  },
 };
 
 const configOnlyWarn = {
-    plugins: {
-        onlyWarn,
-    },
+  plugins: {
+    onlyWarn,
+  },
 };
 
 const configStylistic = stylistic.configs.customize({
-    semi: true,
+  semi: true,
 });
 
 export const eslintConfigNuxt = [
-    customEslintRules,
-    customIgnores,
-    configUnicornRecommended,
-    customUnicornRules,
-    configPerfectionist,
-    configOnlyWarn,
-    configStylistic,
+  customEslintRules,
+  customIgnores,
+  configUnicornRecommended,
+  customUnicornRules,
+  configPerfectionist,
+  configOnlyWarn,
+  configStylistic,
 ];
 
 /**
@@ -114,16 +114,16 @@ export const eslintConfigNuxt = [
  * @see https://typescript-eslint.io/blog/changes-to-consistent-type-imports-with-decorators/
  */
 const configParser = {
-    languageOptions: {
-        parser: typescriptParser,
-        parserOptions: {
-            emitDecoratorMetadata: true,
-            experimentalDecorators: true,
-        },
+  languageOptions: {
+    parser: typescriptParser,
+    parserOptions: {
+      emitDecoratorMetadata: true,
+      experimentalDecorators: true,
     },
+  },
 };
 
 export const eslintConfigCommon = createConfigForNuxt().append([
-    ...eslintConfigNuxt,
-    configParser,
+  ...eslintConfigNuxt,
+  configParser,
 ]);
