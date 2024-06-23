7795c40c284135ee64a2707a17eb7b9e43f3459e
mitou <xmitoux@gmail.com>
2024-06-23 19:15:24 +0900

    feat(backend): prismaサービスを作成

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,13 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { Logger } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
+import { PrismaClientExceptionFilter } from './common/filters/prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
 
   const logger = app.get(Logger);
   app.useLogger(logger);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   await app.listen(3000, () => {
     logger.log(`Server listening on port:3000 NODE_ENV=${process.env.NODE_ENV}`);
