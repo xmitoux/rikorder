7e64d6f50f7238d9ed54471f7e1e39f848cd8598
mitou <xmitoux@gmail.com>
2024-07-02 20:41:01 +0900

    feat(frontend): フッターを作成

--- apps/frontend/layouts/default.vue
+++ apps/frontend/layouts/default.vue
@@ -16,6 +16,25 @@
     </q-page-container>
 
     <q-footer class="bg-pink-2 text-white">
+      <q-toolbar>
+        <q-icon name="mdi-home-heart" size="md" />
+
+        <q-space />
+
+        <q-icon flat name="mdi-image-multiple" size="md" />
+
+        <q-space />
+
+        <q-icon flat name="mdi-clipboard-text" size="md" />
+
+        <q-space />
+
+        <q-icon flat name="mdi-crown" size="md" />
+
+        <q-space />
+
+        <q-icon flat name="mdi-chart-bar" size="md" />
+      </q-toolbar>
     </q-footer>
   </q-layout>
 </template>
