620b3eac05d8e4154a28f2752574b744b96852f0
mitou <xmitoux@gmail.com>
2024-07-03 23:08:15 +0900

    feat(frontend): 梨子ちゃんライブラリに登録/フィルタボタンを設置

--- apps/frontend/src/pages/library/index.vue
+++ apps/frontend/src/pages/library/index.vue
@@ -3,10 +3,22 @@
 
 <template>
   <div>
-    <ToggleButton
-      label1="画像" label2="タグ"
-      mdi1="mdi-image-outline" mdi2="mdi-tag-outline"
-    />
+    <div class="row justify-end">
+      <div class="col-8">
+        <ToggleButton
+          label1="画像" label2="タグ"
+          mdi1="mdi-image-outline" mdi2="mdi-tag-outline"
+        />
+      </div>
+
+      <div class="col-1 text-center q-px-xs">
+        <q-btn color="dark" flat icon="mdi-filter-menu-outline" round />
+      </div>
+
+      <div class="col-1 text-center q-px-xs">
+        <q-btn color="dark" flat icon="mdi-plus" round />
+      </div>
+    </div>
   </div>
 </template>
 
