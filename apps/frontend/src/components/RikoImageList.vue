2a150ed57d43f87ce99f97fcd862ead970ff510d
mitou <xmitoux@gmail.com>
2024-07-04 00:40:22 +0900

    feat(frontend): 梨子ちゃん画像一覧表示用コンポーネントを作成

---
+++ apps/frontend/src/components/RikoImageList.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = defineProps<{
+  images: { id: number; url: string }[];
+}>();
+
+const emit = defineEmits<{
+  click: [imageId: number];
+}>();
+</script>
+
+<template>
+  <div class="row text-center">
+    <div v-for="image in props.images" :key="image.id" class="col-6">
+      <q-img
+        class="image-container q-mt-md"
+        spinner-color="pink-2"
+        :src="image.url"
+        @click="emit('click', image.id)"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@use "~/assets/scss/_variables.scss" as var;
+
+.image-container {
+  width: var.$riko-image-thumbnail-size;
+  height: var.$riko-image-thumbnail-size;
+  border-radius: var.$riko-image-border-radius;
+}
+</style>
