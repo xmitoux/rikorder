fe4a1685034e0a63f9014c41f0a9329cf77e2251
mitou <xmitoux@gmail.com>
2024-07-13 13:38:16 +0900

    feat(frontend): ホーム画面に情報エリアのガワを追加

---
+++ apps/frontend/src/components/ui-parts/label/LabelChip.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{ label: string }>();
+</script>
+
+<template>
+  <q-chip color="white" :ripple="false">
+    {{ label }}
+  </q-chip>
+</template>
+
+<style scoped lang="scss">
+
+</style>
--- apps/frontend/src/pages/index.vue
+++ apps/frontend/src/pages/index.vue
@@ -4,8 +4,54 @@
 
 <template>
   <div>
-    ホーム画面
+    <!-- 情報エリア -->
+    <div class="q-ml-sm q-mb-lg">
+      <UISectionLabel class="q-mb-md" label="情報" />
+
+      <!-- q-cardのborder-radius変更用にdivで囲む -->
+      <div class="q-mb-lg card-border-radius">
+        <q-card bordered class="q-py-sm bg-pink-1 no-border border-radius-inherit" flat>
+          <q-card-section horizontal>
+            <q-card-section class="col-6 q-px-sm q-py-sm text-center">
+              <UILabelChip label="前回日時/目標" />
+
+              <div class="q-mb-xs text-h6">
+                9/19(木) 9:19
+              </div>
+
+              <div class="text-subtitle">
+                9時間19分 経過
+              </div>
+            </q-card-section>
+
+            <q-separator inset vertical />
+
+            <q-card-section class="col-6 q-px-sm q-py-sm text-center">
+              <UILabelChip label="今月回数" />
+
+              <div class="q-mt-sm text-h6">
+                19回
+              </div>
+            </q-card-section>
+          </q-card-section>
+
+          <q-separator inset />
+
+          <q-card-section class="q-px-sm q-py-sm text-center">
+            <UILabelChip label="今月の記録/目標" />
+
+            <div class="text-h6">
+              19回 / 919回
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.card-border-radius {
+  border-radius: 10px;
+}
+</style>
