0ba984b654d13ac85b29f32effd768bcff1b9dea
mitou <xmitoux@gmail.com>
2024-07-15 15:59:43 +0900

    feat(home): モード別画像取得APIをフロントに実装

--- apps/frontend/src/types/rikord-mode.ts
+++ apps/frontend/src/types/rikord-mode.ts
@@ -1,13 +1,13 @@
 import type { RIKORD_MODE_VALUES } from '~/constants/rikord-mode';
 
 // Rikordモード値をtype化
-export type RikordModeValue = typeof RIKORD_MODE_VALUES[keyof typeof RIKORD_MODE_VALUES];
+export type RikordModeIdValue = typeof RIKORD_MODE_VALUES[keyof typeof RIKORD_MODE_VALUES];
 
 // Rikordモード名をtype化
 export type RikordModeName = keyof typeof RIKORD_MODE_VALUES;
 
 // Rikordモードの各情報のtype
 export type RikordModeInfo = {
-  id: RikordModeValue;
+  id: RikordModeIdValue;
   modeName: RikordModeName;
 };
--- apps/frontend/src/utils/api/riko-images/index.ts
+++ apps/frontend/src/utils/api/riko-images/index.ts
@@ -1,9 +1,15 @@
 import type { RikoImageCreateDto, RikoImageEntityResponse } from '@repo/db';
 
+import type { RikordModeIdValue } from '~/types/rikord-mode';
+
 export const createRikoImageApi = (body: RikoImageCreateDto) => {
   return $fetch<RikoImageEntityResponse>('/api/riko-images', { body, method: 'post' });
 };
 
 export const findRikoImagesApi = () => {
   return useFetch<RikoImageEntityResponse[]>('/api/riko-images', { method: 'get' });
+};
+
+export const findRikoImagesByRikordModeIdApi = (rikordModeId: RikordModeIdValue) => {
+  return useFetch<RikoImageEntityResponse[]>(`/api/riko-images/find-by-rikord-mode/${rikordModeId}`, { method: 'get' });
 };
