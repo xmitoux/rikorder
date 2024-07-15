6b6a373db8b89a79b56bac351599cba3a791a717
mitou <xmitoux@gmail.com>
2024-07-15 12:35:23 +0900

    chore(backend): Prismaのクエリログ出力設定

--- apps/backend/src/common/services/prisma.service.ts
+++ apps/backend/src/common/services/prisma.service.ts
@@ -1,10 +1,39 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, Logger } from '@nestjs/common';
 
-import { PrismaClient } from '@repo/db';
+import { Prisma, PrismaClient } from '@repo/db';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel> implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'event', level: 'info' },
+        { emit: 'event', level: 'warn' },
+        { emit: 'event', level: 'error' },
+      ],
+    });
+  }
+
   async onModuleInit() {
+    this.$on('query', (event) => {
+      this.logger.log(`[Query] ${event.query} [Params] ${event.params} [Duration] ${event.duration} ms`);
+    });
+
+    this.$on('info', (event) => {
+      this.logger.log(`message: ${event.message}`);
+    });
+
+    this.$on('error', (event) => {
+      this.logger.log(`error: ${event.message}`);
+    });
+
+    this.$on('warn', (event) => {
+      this.logger.log(`warn: ${event.message}`);
+    });
+
     await this.$connect();
   }
 }
