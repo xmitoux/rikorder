02b4612fd54f4edb2cdaa06bff9bc0f1ec6f323e
mitou <xmitoux@gmail.com>
2024-07-15 14:05:02 +0900

    chore(backend): Prismaのクエリログ出力設定変更
    - クエリのみ出力するよう設定
    - エラーはフィルタで出している
    - タイムスタンプとパスを返す

--- apps/backend/src/common/services/prisma.service.ts
+++ apps/backend/src/common/services/prisma.service.ts
@@ -7,31 +7,12 @@ export class PrismaService extends PrismaClient<Prisma.PrismaClientOptions, Pris
   private readonly logger = new Logger(PrismaService.name);
 
   constructor() {
-    super({
-      log: [
-        { emit: 'event', level: 'query' },
-        { emit: 'event', level: 'info' },
-        { emit: 'event', level: 'warn' },
-        { emit: 'event', level: 'error' },
-      ],
-    });
+    super({ log: [{ emit: 'event', level: 'query' }] });
   }
 
   async onModuleInit() {
     this.$on('query', (event) => {
       this.logger.log(`[Query] ${event.query} [Params] ${event.params} [Duration] ${event.duration} ms`);
-    });
-
-    this.$on('info', (event) => {
-      this.logger.log(`message: ${event.message}`);
-    });
-
-    this.$on('error', (event) => {
-      this.logger.log(`error: ${event.message}`);
-    });
-
-    this.$on('warn', (event) => {
-      this.logger.log(`warn: ${event.message}`);
     });
 
     await this.$connect();
