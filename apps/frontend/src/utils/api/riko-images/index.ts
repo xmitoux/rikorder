d46ad27b44aa7e8a99b0e4df1e0a34e0757cb275
mitou <xmitoux@gmail.com>
2024-07-21 22:37:30 +0900

    delete: 未使用のAPIを削除

--- apps/frontend/src/utils/api/riko-images/index.ts
+++ apps/frontend/src/utils/api/riko-images/index.ts
@@ -1,12 +1,8 @@
 import { FetchError } from 'ofetch';
 
-import type { RikoImageCreateDto, RikoImageEntityResponse } from '@repo/db';
+import type { RikoImageEntityResponse } from '@repo/db';
 
 import type { RikordModeIdValue } from '~/types/rikord-mode';
-
-export const createRikoImageApi = (body: RikoImageCreateDto) => {
-  return $fetch<RikoImageEntityResponse>('/api/riko-images', { body, method: 'post' });
-};
 
 export const findRikoImagesApi = () => {
   return useFetch<RikoImageEntityResponse[]>('/api/riko-images', { method: 'get' });
