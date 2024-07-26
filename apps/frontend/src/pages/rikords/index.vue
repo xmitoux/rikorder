0c451cb6dba10f4c27500ab86e8496c403986ab6
mitou <xmitoux@gmail.com>
2024-07-27 00:45:21 +0900

    feat(rikords): 画面にrikords取得処理を実装
    画面にはとりあえず年月セレクタだけ配置

--- apps/frontend/src/pages/rikords/index.vue
+++ apps/frontend/src/pages/rikords/index.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+import type { RikordEntityResponse, SearchRikordsDto } from '@repo/db';
 
+const date = new Date();
+const yearMonth = ref<SearchRikordsDto>({
+  year: date.getFullYear(),
+  month: date.getMonth() + 1,
+});
+
+const rikords = ref<RikordEntityResponse[]>([]);
+
+watchEffect(async () => {
+  rikords.value = await searchRikordsApi(yearMonth.value).catch(() => {
+    console.error('Rikord一覧取得に失敗しました。');
+    return [];
+  });
+});
 </script>
 
 <template>
   <div>
-    Rikord一覧画面
+    <UISelectYearMonth v-model:year-month="yearMonth" />
   </div>
 </template>
 
