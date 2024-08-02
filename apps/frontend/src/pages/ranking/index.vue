a81f0cfb1e067bc5761958352c9b9a4f50e5bea5
Masato Ito <m-itou@yournet-jp.com>
2024-08-02 12:01:31 +0900

    feat: ランキング一覧コンポーネントを作成
    テストデータで表示確認

--- apps/frontend/src/pages/ranking/index.vue
+++ apps/frontend/src/pages/ranking/index.vue
@@ -1,10 +1,20 @@
+<!-- 👑ランキング画面 -->
 <script setup lang="ts">
+const store = useRikordModeStore();
+const { currentRikordMode } = storeToRefs(store);
 
+const rankingList = ref([]);
+rankingList.value = [
+  { rank: 1, rankValue: 1_366_500, rikoImage: { url: 'https://bibjqyafzulueizuahxw.supabase.co/storage/v1/object/public/riko-images-dev/1721315402017_30Sakurauchi-Riko-Water-Symphony-UR-hZpU4Z.jpg' } },
+  { rank: 2, rankValue: 4320, rikoImage: { url: 'https://bibjqyafzulueizuahxw.supabase.co/storage/v1/object/public/riko-images-dev/1721315402017_30Sakurauchi-Riko-Water-Symphony-UR-hZpU4Z.jpg' } },
+  { rank: 3, rankValue: 610, rikoImage: { url: 'https://bibjqyafzulueizuahxw.supabase.co/storage/v1/object/public/riko-images-dev/1721315402017_30Sakurauchi-Riko-Water-Symphony-UR-hZpU4Z.jpg' } },
+  { rank: 4, rankValue: 61, rikoImage: { url: 'https://bibjqyafzulueizuahxw.supabase.co/storage/v1/object/public/riko-images-dev/1721315402017_30Sakurauchi-Riko-Water-Symphony-UR-hZpU4Z.jpg' } },
+];
 </script>
 
 <template>
   <div>
-    ランキング画面
+    <RankingList :items="rankingList" :rikord-mode="currentRikordMode.modeName" />
   </div>
 </template>
 
