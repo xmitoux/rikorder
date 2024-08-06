12ee4b0e954cee045e26d71dd6aededff341533c
Masato Ito <m-itou@yournet-jp.com>
2024-08-06 14:28:00 +0900

    feat(frontend): LoadingIndicatorを設定
    https://nuxt.com/docs/api/components/nuxt-loading-indicator

--- apps/frontend/src/app.vue
+++ apps/frontend/src/app.vue
@@ -7,7 +7,7 @@ const NuxtPwaManifest = process.env.NODE_ENV === 'production'
 <template>
   <div>
     <component :is="NuxtPwaManifest" />
-
+    <NuxtLoadingIndicator color="repeating-linear-gradient(to right,#FCE4EC 0%,#F06292 50%,#E91E62 100%)" />
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
